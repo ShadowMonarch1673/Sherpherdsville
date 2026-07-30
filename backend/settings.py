4ddba95147b66989710a66e99f63fe27952adfa5
Joshua-Martin <joshsackey123@gmail.com>
2026-07-30 23:39:56 +0000

    Add CSRF_TRUSTED_ORIGINS for Railway Domain

--- backend/settings.py
+++ backend/settings.py
@@ -34,6 +34,9 @@ DEBUG = env("DEBUG")
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
+CSRF_TRUSTED_ORIGINS = [
+    f"https://{host}" for host in ALLOWED_HOSTS if host not in ("127.0.0.1", "localhost")
+]
 
 # Application definition
 
